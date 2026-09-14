96fb9a3be0a186652d215b8b7d421e7a4ec12c38
Yolangema <yolangemaa@gmail.com>
2026-09-14 23:19:38 +0300

    feat: Задания 1 и 2 ЛР12 (EventEmitter сервер и логгер)

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const http = require('http');
-const p = (16 * Math.atan(1/5) - 4 * Math.atan(1/239)).toFixed(17);
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<h1>Парфёнок Иван Николаевич<br>477<br>${p}</h1>');
+const { EventEmitter } = require('events');
+const logger = require('./logger');
+
+class AppServer extends EventEmitter {
+    constructor() {
+        super();
+        this.server = null;
+    }
+
+    start(port) {
+        this.server = http.createServer((req, res) => {
+            // Задание 1.2: генерация события при запросе
+            this.emit('request:received', {
+                url: req.url,
+                method: req.method
+            });
+
+            // Задание 1.4: ответ на все запросы
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end('Hello from Event-Driven Server!');
+        });
+
+        this.server.listen(port, () => {
+            this.emit('server:started', port);
+        });
+    }
+
+    stop() {
+        if (this.server) {
+            this.server.close(() => {
+                this.emit('server:stopped');
+            });
+        }
+    }
+}
+
+const app = new AppServer();
+
+// Задание 1.3: Регистрация обработчиков
+app.on('server:started', (port) => {
+    console.log(`🚀 Сервер запущен на порту ${port}`);
 });
 
-server.listen(3000, () => {
-  console.log('Сервер запущен на http://localhost:3000');
+app.on('request:received', (request) => {
+    console.log(`📨 Получен запрос: ${request.method} ${request.url}`);
 });
+
+app.on('server:stopped', () => {
+    console.log('🛑 Сервер остановлен');
+});
+
+// Задание 2: Подключение логгера
+logger.setupLogger(app);
+
+// Запуск
+app.start(3000);
